4e364f9e4d799f1767ddcb0e70bf5a41cc0d9211
TGWorkPlace <DramaShip.org@gmail.com>
2026-06-27 16:15:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -240,7 +240,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/DramaShip">{file_name}</a></b>\n<b>✪ size- {file_size}</b>\n\n<b>Have A Nice Day 💖</b>\n\n<b>Bot: @DramaShip1bot</b>\n\n<b>⚜️ Powered By : <a href="https://t.me/DramaShip">@ᴅʀᴀᴍᴀꜱʜɪᴘ</a></b>"""
+    CAPTION = """<b><a href="https://t.me/DramaShip">{file_name}</a></b>\n<b>✪ size- {file_size}</b>\n\n<b>Have A Nice Day 💖</b>\n\n<b>Bot:\n⮕ @DramaShip1bot\n⮕ @AnyDramaBot</b>\n\n<b>⚜️ Powered By : <a href="https://t.me/DramaShip">@ᴅʀᴀᴍᴀꜱʜɪᴘ</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
